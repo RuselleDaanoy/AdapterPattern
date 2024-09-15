5d6bd7f85891f2c2e39ee573aab597c773c85044
Ruselle Daanoy <133234541+RuselleDaanoy@users.noreply.github.com>
2024-09-15 10:43:27 +0000

    AdapterPattern

---
+++ Appliances.java
@@ -0,0 +1,16 @@
+public class Appliances {
+    public static void main(String[] args) {
+
+        Laptop laptop = new Laptop();
+        Refrigerator refrigerator = new Refrigerator();
+        SmartphoneCharger smartphoneCharger = new SmartphoneCharger();
+
+        PowerOutlet laptopAdapter = new LaptopAdapter(laptop);
+        PowerOutlet refrigeratorAdapter = new RefrigeratorAdapter(refrigerator);
+        PowerOutlet smartphoneAdapter = new SmartphoneAdapter(smartphoneCharger);
+
+        System.out.println(laptopAdapter.plugIn());
+        System.out.println(refrigeratorAdapter.plugIn()); 
+        System.out.println(smartphoneAdapter.plugIn());   
+    }
+}
---
+++ SmartphoneAdapter.java
@@ -0,0 +1,12 @@
+class SmartphoneAdapter implements PowerOutlet {
+    private SmartphoneCharger charger;
+
+    public SmartphoneAdapter(SmartphoneCharger charger) {
+        this.charger = charger;
+    }
+
+    @Override
+    public String plugIn() {
+        return charger.chargePhone(); 
+    }
+}
